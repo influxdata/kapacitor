0a2e6f9b39f88cc884f5b0a2c86427dbe67d1239
Nathaniel Cook <nvcook42@gmail.com>
2016-06-09 10:36:53 -0600

    update proto for udf

--- udf/udf.pb.go
+++ udf/udf.pb.go
@@ -1218,6 +1218,8 @@ func init() {
 	proto.RegisterEnum("udf.ValueType", ValueType_name, ValueType_value)
 }
 
+func init() { proto.RegisterFile("udf.proto", fileDescriptor0) }
+
 var fileDescriptor0 = []byte{
 	// 1064 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xc4, 0x56, 0xef, 0x72, 0xdb, 0x44,
