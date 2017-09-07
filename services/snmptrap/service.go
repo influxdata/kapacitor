45d1b41b7ea51122b11b03a1c69d091e718b3f01
Nicholas Radonicich <nicholas.radonicich@cogeco.com>
2017-09-07 19:22:35 -0400

    Updating snmptrap service to contain sysUpTime OID in trap

--- services/snmptrap/service.go
+++ services/snmptrap/service.go
@@ -133,8 +133,9 @@ func (s *Service) Trap(trapOid string, dataList []Data) error {
 		return errors.Wrapf(err, "invalid trap Oid %q", trapOid)
 	}
 	varBinds := snmpgo.VarBinds{
-		snmpgo.NewVarBind(snmpgo.OidSnmpTrap, oid),
-	}
+		snmpgo.NewVarBind(snmpgo.OidSysUpTime, snmpgo.NewTimeTicks(1000))
+        }
+        varBinds = append(varBinds, snmpgo.NewVarBind(snmpgo.OidSnmpTrap, oid))
 
 	// Add Data
 	for _, data := range dataList {
