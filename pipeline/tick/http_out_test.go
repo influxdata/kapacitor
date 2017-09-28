ee38190e352c63a0517d97ead315678c46b629ec
Chris Goller <goller@gmail.com>
2017-09-28 14:28:45 -0500

    Add test for pipeline tick http out node

---
+++ pipeline/tick/http_out_test.go
@@ -0,0 +1,24 @@
+package tick_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestHTTPOut(t *testing.T) {
+	pipe, _, from := StreamFrom()
+	from.HttpOut("There is never any ending to Paris – Hemingway")
+	got, err := PipelineTick(pipe)
+	if err != nil {
+		t.Fatalf("Unexpected error building pipeline %v", err)
+	}
+
+	want := `stream
+    |from()
+    |httpOut('There is never any ending to Paris – Hemingway')
+`
+	if got != want {
+		t.Errorf("TestSample = %v, want %v", got, want)
+		fmt.Println(got) // print is helpful to get the correct format.
+	}
+}
