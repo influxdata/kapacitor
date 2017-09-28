c9588cad83f22fc777d2044252dac70cc0396a90
Chris Goller <goller@gmail.com>
2017-09-28 09:58:22 -0500

    Add test for pipeline tick state tracking

---
+++ pipeline/tick/state_tracking_test.go
@@ -0,0 +1,77 @@
+package tick_test
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/influxdata/kapacitor/tick/ast"
+)
+
+func TestStateDuration(t *testing.T) {
+	pipe, _, from := StreamFrom()
+	lambda := &ast.LambdaNode{
+		Expression: &ast.BinaryNode{
+			Left: &ast.ReferenceNode{
+				Reference: "cpu",
+			},
+			Right: &ast.StringNode{
+				Literal: "cpu-total",
+			},
+			Operator: ast.TokenNotEqual,
+		},
+	}
+
+	sd := from.StateDuration(lambda)
+	sd.As = "AKA"
+	sd.Unit = time.Minute
+
+	got, err := PipelineTick(pipe)
+	if err != nil {
+		t.Fatalf("Unexpected error building pipeline %v", err)
+	}
+
+	want := `stream
+    |from()
+    |stateDuration(lambda: "cpu" != 'cpu-total')
+        .as('AKA')
+        .unit(1m)
+`
+	if got != want {
+		t.Errorf("TestStateDuration = %v, want %v", got, want)
+		fmt.Println(got) // print is helpful to get the correct format.
+	}
+}
+
+func TestStateCount(t *testing.T) {
+	pipe, _, from := StreamFrom()
+	lambda := &ast.LambdaNode{
+		Expression: &ast.BinaryNode{
+			Left: &ast.ReferenceNode{
+				Reference: "cpu",
+			},
+			Right: &ast.StringNode{
+				Literal: "cpu-total",
+			},
+			Operator: ast.TokenNotEqual,
+		},
+	}
+
+	sd := from.StateCount(lambda)
+	sd.As = "AKA"
+
+	got, err := PipelineTick(pipe)
+	if err != nil {
+		t.Fatalf("Unexpected error building pipeline %v", err)
+	}
+
+	want := `stream
+    |from()
+    |stateCount(lambda: "cpu" != 'cpu-total')
+        .as('AKA')
+`
+	if got != want {
+		t.Errorf("TestStateCount = %v, want %v", got, want)
+		fmt.Println(got) // print is helpful to get the correct format.
+	}
+}
