60b55a3b3548ef7763efbea227b6f205e7616b76
Chris Goller <goller@gmail.com>
2017-09-28 15:31:25 -0500

    Add test for pipeline tick batch node

---
+++ pipeline/tick/batch_test.go
@@ -0,0 +1,22 @@
+package tick_test
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBatch(t *testing.T) {
+	pipe, _, _ := BatchQuery("select cpu_usage from cpu")
+	got, err := PipelineTick(pipe)
+	if err != nil {
+		t.Fatalf("Unexpected error building pipeline %v", err)
+	}
+
+	want := `batch
+    |query('select cpu_usage from cpu')
+`
+	if got != want {
+		t.Errorf("TestBatch = %v, want %v", got, want)
+		fmt.Println(got) // print is helpful to get the correct format.
+	}
+}
