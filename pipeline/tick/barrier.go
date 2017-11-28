c0f7d0898188ef693560cb7bbc859622b4d46ad7
Min Pae <sputnik13@gmail.com>
2017-11-28 11:07:05 -0800

    add barrier ast

---
+++ pipeline/tick/barrier.go
@@ -0,0 +1,28 @@
+package tick
+
+import (
+	"github.com/influxdata/kapacitor/pipeline"
+	"github.com/influxdata/kapacitor/tick/ast"
+)
+
+// BarrierNode converts the window pipeline node into the TICKScript AST
+type BarrierNode struct {
+	Function
+}
+
+// NewBarrierNode creates a Barrier function builder
+func NewBarrierNode(parents []ast.Node) *BarrierNode {
+	return &BarrierNode{
+		Function{
+			Parents: parents,
+		},
+	}
+}
+
+// Build creates a window ast.Node
+func (n *BarrierNode) Build(b *pipeline.BarrierNode) (ast.Node, error) {
+	n.Pipe("barrier").
+		Dot("idle", b.Idle).
+		Dot("period", b.Period)
+	return n.prev, n.err
+}
