97b6fd3289c0a5062dcbc8dace7ac88887d23e3e
J. Emrys Landivar <landivar@gmail.com>
2021-03-24 15:57:31 -0500

    build: fix build system (#2511)

---
+++ tools.go
@@ -0,0 +1,12 @@
+// Tools is for building out tools so mod can version them, add any tools you need at runtime to this file as _ imports.
+// This is to follow along with best practices for mod.  https://github.com/golang/go/issues/25922
+
+// +build tools
+
+package kapacitor
+
+import (
+	_ "github.com/benbjohnson/tmpl"
+	_ "github.com/golang/protobuf/protoc-gen-go"
+	_ "github.com/mailru/easyjson/easyjson"
+)
