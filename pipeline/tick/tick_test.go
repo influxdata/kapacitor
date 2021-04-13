95e24ef5bb7b6ca804389aae29a1c2a0c61adf81
j. Emrys Landivar (docmerlin) <landivar@gmail.com>
2021-04-13 11:37:41 -0500

    test: fix TestTrickle ast test

--- pipeline/tick/tick_test.go
+++ pipeline/tick/tick_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/google/go-cmp/cmp"
+
 	"github.com/influxdata/kapacitor/pipeline"
 	"github.com/influxdata/kapacitor/pipeline/tick"
 	"github.com/influxdata/kapacitor/tick/stateful"
@@ -116,7 +118,7 @@ func PipelineTickTestHelper(t *testing.T, pipe *pipeline.Pipeline, want string, 
 	}
 
 	if got != want {
-		t.Errorf("unexpected TICKscript:\ngot:\n%v\nwant:\n%v\n", got, want)
+		t.Errorf("unexpected TICKscript:\n %s", cmp.Diff(got, want))
 		t.Log(got) // print is helpful to get the correct format.
 	}
 
--- pipeline/tick/trickle_test.go
+++ pipeline/tick/trickle_test.go
@@ -2,13 +2,18 @@ package tick_test
 
 import (
 	"testing"
+	"time"
 )
 
 func TestTrickle(t *testing.T) {
 	pipe, _, from := StreamFrom()
-	from.Trickle()
+	w := from.Window()
+	w.Every = time.Second
+	w.Trickle()
 	want := `stream
     |from()
+    |window()
+        .every(1s)
     |trickle()
 `
 	PipelineTickTestHelper(t, pipe, want)
